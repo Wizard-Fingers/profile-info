fd9e886e7920a5a117fc136942e61a544a13dcf6
Repo <art@beardigitalstudios.com>
2024-02-14 16:23:31 +0000

    Profile Info Project Iungo - added a function to send friend requests

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,58 @@
+"use client";
+import React, { useState } from "react";
 import ProfileInfo from "@/components/ProfileInfo.jsx";
 
 export default function Home() {
+  const [friendRequests, setFriendRequests] = useState([]);
+
+  function handleFriendRequest(profile) {
+    setFriendRequests([...friendRequests, profile]);
+  }
+
+  function manyProfiles() {
+    const profiles = [];
+    for (let i = 0; i < 20; i++) {
+      profiles.push(
+        <ProfileInfo
+          key={i}
+          onFriendRequest={(profile) => handleFriendRequest(profile)}
+        />
+      );
+    }
+    return profiles;
+  }
+
   return (
-    <div className="container mx-auto">
-      <h1 className="text-3xl font-bold text-center mt-8">
-        Profile Search Results
-      </h1>
-      {/* search bar I'm assuming? */}
-      <div>
-        <ProfileInfo />
-        <ProfileInfo />
-        <ProfileInfo />
-        <ProfileInfo />
+    <main className="flex">
+      <aside className="w-[15rem] bg-cyan-50 p-4 h-full m-4 text-center">
+        <h2 className="text-lg font-bold mb-4 text-cyan-800">
+          Friend Requests pending:
+        </h2>
+        <ul>
+          {friendRequests.map((profile, index) => (
+            <li key={index}>
+              {profile.name.first} {profile.name.last}
+            </li>
+          ))}
+        </ul>
+      </aside>
+      <div className="container mx-auto flex-1">
+        <nav>
+          <div className="bg-cyan-50 p-4">
+            <h1 className="text-2xl font-bold text-center text-cyan-800">
+              Profile Search
+            </h1>
+          </div>
+        </nav>
+        <div className="mt-8">
+          <h1 className="text-3xl font-bold text-center">
+            Profile Search Results
+          </h1>
+          <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 justify-center items-center mt-8">
+            {manyProfiles()}
+          </div>
+        </div>
       </div>
-    </div>
+    </main>
   );
 }
--- src/components/ProfileInfo.jsx
+++ src/components/ProfileInfo.jsx
@@ -1,8 +1,8 @@
-"use client";
+// "use client";
 import React, { useState, useEffect } from "react";
 import Image from "next/image";
 
-const ProfileInfo = () => {
+const ProfileInfo = ({ onFriendRequest }) => {
   const [profile, setProfile] = useState(null);
 
   useEffect(() => {
@@ -11,29 +11,39 @@ const ProfileInfo = () => {
       .then((data) => setProfile(data.results[0]));
   }, []);
 
+  function handleFriendRequest() {
+    if (profile) {
+      onFriendRequest(profile);
+    }
+  }
+
   return (
-    <div className="flex items-center justify-center my-10">
+    <div className="max-w-md w-full bg-white shadow-lg rounded-lg overflow-hidden p-4 border-4 border-cyan-50">
       {profile && (
-        <div className="max-w-md w-full bg-white shadow-lg rounded-lg overflow-hidden">
-          <div className="px-4 py-4">
-            <Image
-              width={100}
-              height={100}
-              className="w-24 h-24 rounded-full mx-auto"
-              src={profile.picture.large}
-              alt={`${profile.name.first} ${profile.name.last}`}
-            />
-            <div className="text-center mt-4">
-              <h1 className="text-lg font-semibold text-gray-800">
-                {profile.name.first} {profile.name.last}
-              </h1>
-              <p className="text-sm text-gray-600 mt-1">
-                Age: {profile.dob.age}
-              </p>
-              <p className="text-sm text-gray-600 mt-1">
-                Location: {profile.location.city}, {profile.location.country}
-              </p>
-            </div>
+        <div>
+          <Image
+            width={100}
+            height={100}
+            className="w-24 h-24 rounded-full mx-auto border-4 border-cyan-50 drop-shadow-lg"
+            src={profile.picture.large}
+            alt={`${profile.name.first} ${profile.name.last}`}
+          />
+          <div className="text-center mt-4">
+            <h1 className="text-lg font-semibold text-slate-900">
+              {profile.name.first} {profile.name.last}
+            </h1>
+            <p className="text-sm text-slate-600 mt-1">
+              Age: {profile.dob.age}
+            </p>
+            <p className="text-sm text-slate-600 mt-1">
+              Location: {profile.location.city}, {profile.location.country}
+            </p>
+            <button
+              className="bg-cyan-500 text-white px-4 py-2 rounded-lg mt-4 hover:bg-cyan-600 transition-colors duration-300 ease-in-out"
+              onClick={handleFriendRequest}
+            >
+              Send Friend Request
+            </button>
           </div>
         </div>
       )}
